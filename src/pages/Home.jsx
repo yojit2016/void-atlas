65342c0a20e74ab79650ce12f78a2c292f32bf12
yojit2016 <72664501+yojit2016@users.noreply.github.com>
2026-06-27 01:22:28 +0530

    refactor: remove legacy HUD and carousel from home view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,9 @@
-import Loader from "../components/ui/Loader";
-import ObservatoryHUD from "../components/hud/ObservatoryHUD";
 import SceneCanvas from "../components/canvas/SceneCanvas";
-import { useCosmicData } from "../hooks/useCosmicData";
 
-export default function Home(){
-  const {images, loading}= useCosmicData();
-
-  if (loading) {
-    return (
-      <Loader />
-    );
-  }
-
+export default function Home() {
   return (
     <section className="relative h-screen overflow-hidden">
       <SceneCanvas />
-      <div className="relative z-10">
-        <ObservatoryHUD count={images.length} />
-      </div>
     </section>
   );
 }
