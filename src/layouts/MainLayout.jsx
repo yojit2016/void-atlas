7ecb5bfcd2e8067b925a15ed3287fbf654ac969f
yojit2016 <72664501+yojit2016@users.noreply.github.com>
2026-07-04 21:15:43 +0530

    fix: remove legacy 2D starfield, Three.js StarField3D is now sole renderer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,13 @@
 import Navbar from '../components/ui/Navbar'
 import StarCursor from '../components/ui/StarCursor'
 import Home from '../pages/Home'
-import StarfieldCanvas from '../components/cosmic/StarfieldCanvas'
+
 
 export default function MainLayout() {
   return (
     <div className="relative min-h-screen overflow-hidden bg-black text-white">
       {/* Background: Starfield at z-0 */}
-      <StarfieldCanvas />
+      
       
       {/* Atmosphere: Subtle vignette and atmospheric haze throughout */}
       <div className="fixed inset-0 z-5 pointer-events-none bg-radial-at-center from-transparent via-black/5 to-black/20" />
